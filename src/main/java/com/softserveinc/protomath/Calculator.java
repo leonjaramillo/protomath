eb96263eda64d048db2c1fcbd6c23429adca1fa2
León Jaramillo <leonjaramillo@gmail.com>
2022-11-21 10:12:57 -0500

    Update Calculator.java

--- src/main/java/com/softserveinc/protomath/Calculator.java
+++ src/main/java/com/softserveinc/protomath/Calculator.java
@@ -3,7 +3,7 @@ package com.softserveinc.protomath;
 public class Calculator {
 
     public Double square(Double number) {
-        return number * number;
+        return number + number;
     }
     
     public Double cube(Double number) {
